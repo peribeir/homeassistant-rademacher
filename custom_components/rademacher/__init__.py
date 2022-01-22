13f52be919d18a7ff13900023823b5f8cece9280
Pedro Ribeiro <pedroeusebio@gmail.com>
2022-01-22 21:08:32 +0000

    Fix

--- custom_components/rademacher/__init__.py
+++ custom_components/rademacher/__init__.py
@@ -77,7 +77,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     )
 
     # Backward compatibility
-    entry_options = entry.options.copy()
+    entry_options = {key: entry.options[key] for key in entry.options.keys()}
     if CONF_EXCLUDE not in entry.options:
         if CONF_DEVICES in entry.options:
             entry_options[CONF_EXCLUDE] = [
